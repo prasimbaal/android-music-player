7b7a53079a5166654c89ff20b77145adb2ba90ff
Merrycodesx <148143035+Merrycodesx@users.noreply.github.com>
2024-06-03 12:18:19 +0300

    added the Song Adapter class file

---
+++ app/src/main/java/Song.kt
@@ -0,0 +1,10 @@
+package com.ex.musicplayer
+
+data class Song(
+    val title: String,
+    val artist: String,
+    val path: String,
+    val albumArtUri: String,
+    var isPlaying: Boolean = false // New property to indicate if the song is currently playing
+
+)
